2f2e8285786b964b3e549acf6ef33b2251a12598
2924a67 <5619415@mail.ru>
2022-08-04 00:28:21 +0300

    Add Homework_02

---
+++ Homework_02/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+// int a = 2;
+// int b = 3;
+// int c = 7;
+// int max = a;
+// if (b > a)
+    // max = b;
+// if (c > max)
+    // max = c;
+    // {
+        // Console.WriteLine("c является максимальным числом");
+    // }
+Console.Write("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (b > a)
+
+if (c > b)
+{
+Console.WriteLine("Третье число максимальное");
+}
+
--- Task_00/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿
-
-Console.Write ("Введите целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int square = num * num;
-Console.Write ($"Квадрат целого числа {num} = {square}");
-
